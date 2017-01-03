94abfff3e67d37440aaeb4f2bea000f16507d6ef
Steven Collins <divergentlepton@outlook.com>
2017-01-03 13:07:42 +0000

    added connect tests

--- tests/client.js
+++ tests/client.js
@@ -10,6 +10,7 @@ describe('openraildata-darwin tests', () => {
       expect(process.env.QUEUE).to.not.be.equal('', 'QUEUE env should not be an empty string');
     });
   });
+
   describe('Create a new instance of darwin client', () => {
     it('Expect a valid darwin instance', () => {
       const testDarwin = new Darwin();
@@ -18,6 +19,27 @@ describe('openraildata-darwin tests', () => {
       expect(testDarwin.credentials.port).to.be.equal(61613, 'instance port is incorrect');
       expect(testDarwin.credentials.connectHeaders.login).to.be.equal('d3user', 'instance has the incorrect username');
       expect(testDarwin.credentials.connectHeaders.passcode).to.be.equal('d3password', 'instance has the incorrect username');
+    });
+  });
+
+  describe('Connect to Darwin Server', () => {
+    const testDarwin = new Darwin();
+    it('Expect a valid darwin connection', (done) => {
+      expect(testDarwin).to.be.an('object', 'Resultant should be an object');
+      testDarwin.connect((err) => {
+        expect(err).to.be.equal(null, 'No error should be returned on connection');
+        expect(testDarwin.client).to.not.be.equal(null, 'Client should not be null');
+        done();
+      });
+    });
+    it('Expect an invalid darwin connection', (done) => {
+      expect(testDarwin).to.be.an('object', 'Resultant should be an object');
+      expect(testDarwin.client).to.not.be.equal(null, 'Client should not be null');
+      testDarwin.connect((err) => {
+        expect(err instanceof Error).to.be.equal(true, 'An Error object should be returned');
+        expect(testDarwin.client).to.not.be.equal(null, 'Client should not be null');
+        done();
+      });
     });
   });
 });
